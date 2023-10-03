60fd241a2e2e108b14270b28bca57a4a1e635d7f
vzinchenko <vzinchenko@ptsecurity.com>
2023-10-03 09:36:35 +0700

    Добавила вспомогательный класс для хранения свойств группы

--- test_add_group.py
+++ test_add_group.py
@@ -1,49 +1,73 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from group import Group
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-    
+
     def test_add_group(self):
         wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group(name="gdgfdfgdfg", header="dgdgd", footer="dgdgfdg"))
+        self.open_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group(name="", header="", footer=""))
+        self.open_groups_page(wd)
+        self.logout(wd)
+
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/index.php")
+
+    def login(self, wd, username, password):
         wd.find_element("name", "user").click()
         wd.find_element("name", "user").clear()
-        wd.find_element("name", "user").send_keys("admin")
+        wd.find_element("name", "user").send_keys(username)
         wd.find_element("name", "pass").click()
         wd.find_element("name", "pass").clear()
-        wd.find_element("name", "pass").send_keys("secret")
+        wd.find_element("name", "pass").send_keys(password)
         wd.find_element("css selector", "input[type='submit']").click()
+
+    def open_groups_page(self, wd):
         wd.find_element("link text", "groups").click()
+
+    def create_group(self, wd, group):
+        # создать группу
         wd.find_element("name", "new").click()
+        # заполнить поля группы
         wd.find_element("name", "group_name").click()
         wd.find_element("name", "group_name").clear()
-        wd.find_element("name", "group_name").send_keys("gdgfdfgdfg")
+        wd.find_element("name", "group_name").send_keys(group.name)
         wd.find_element("name", "group_header").click()
         wd.find_element("name", "group_header").clear()
-        wd.find_element("name", "group_header").send_keys("dgdgd")
+        wd.find_element("name", "group_header").send_keys(group.header)
         wd.find_element("name", "group_footer").click()
         wd.find_element("name", "group_footer").clear()
-        wd.find_element("name", "group_footer").send_keys("dgdgfdg")
+        wd.find_element("name", "group_footer").send_keys(group.footer)
+        # нажать кнопку создания группы
         wd.find_element("name", "submit").click()
-        wd.find_element("link text", "groups").click()
-        wd.find_element("link text", "Logout").click()
-    
+
+    def logout(self, wd):
+            wd.find_element("link text", "Logout").click()
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
+        try: self.wd.switch_to.alert()
         except NoAlertPresentException as e: return False
         return True
     
