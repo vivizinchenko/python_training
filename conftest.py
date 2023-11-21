9e6b758a3a2a1632987193f403cb26874bba16d9
vzinchenko <vzinchenko@ptsecurity.com>
2023-11-21 23:00:23 +0700

    Добавила проверку текущего состояния сессии

--- conftest.py
+++ conftest.py
@@ -13,14 +13,14 @@ def app(request):
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+    fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
 
     request.addfinalizer(fin)
--- fixture/session.py
+++ fixture/session.py
@@ -1,4 +1,5 @@
 import time
+from selenium.webdriver.common.by import By
 
 
 class SessionHelper:
@@ -21,3 +22,26 @@ class SessionHelper:
         wd = self.app.wd
         wd.find_element("link text", "Logout").click()
         time.sleep(1)
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        lg = wd.find_elements("link text", "Logout")
+        return len(lg) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element(By.XPATH, "//div[@id='top']/form/b").text == "("+username+")"
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
