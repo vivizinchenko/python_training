ddb4bc60d54a26f3a8279fff0970c43e527ae31a
vzinchenko <vzinchenko@ptsecurity.com>
2023-11-23 23:12:50 +0700

    Оптимизировала переходы между страницами

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,6 @@ from fixture.contact import ContactHelper
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -21,7 +20,8 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
+        if not (wd.current_url.endswith("/index.php")):
+            wd.get("http://localhost/addressbook/index.php")
 
     def destroy(self):
         self.wd.quit()
--- fixture/group.py
+++ fixture/group.py
@@ -8,7 +8,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element("link text", "groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements("name", "new")) > 0):
+            wd.find_element("link text", "groups").click()
 
     def create(self, group):
         wd = self.app.wd
